d1b2e3e5587e75b5fb439293c09606cbe8904044
= <=>
2024-07-13 20:47:05 -0300

    feat: Created AutenticateLogin Service

---
+++ src/main/java/com/douginfodev/forumhub/service/AutenticacaoService.java
@@ -0,0 +1,23 @@
+package com.douginfodev.forumhub.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.douginfodev.forumhub.repository.UsuarioRepository;
+
+@Service
+public class AutenticacaoService implements UserDetailsService {
+    
+    @Autowired
+    private UsuarioRepository repository; 
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+       // throw new UnsupportedOperationException("Unimplemented method 'loadUserByUsername'");
+      return repository.findByLogin(username);
+    }
+
+}
