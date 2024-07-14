7f5d4512e1f27a6011e89490bcafad3444905032
= <=>
2024-07-14 00:30:43 -0300

    feat: Created AutenticacaoController

---
+++ src/main/java/com/douginfodev/forumhub/controller/AutenticacaoController.java
@@ -0,0 +1,34 @@
+package com.douginfodev.forumhub.controller;
+
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.douginfodev.forumhub.domain.usuario.DadosAutenticacao;
+
+import org.springframework.web.bind.annotation.PostMapping;
+
+
+@RestController
+@RequestMapping("/login")
+public class AutenticacaoController {
+
+    @Autowired
+    private AuthenticationManager manager;
+
+    @PostMapping
+    @Transactional
+    public ResponseEntity efetuarLogin(@RequestBody @Valid DadosAutenticacao dados){
+        var token = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
+        var authentication = manager.authenticate(token);
+
+        return ResponseEntity.ok().build();
+    }
+}
---
+++ src/main/java/com/douginfodev/forumhub/domain/usuario/DadosAutenticacao.java
@@ -0,0 +1,6 @@
+package com.douginfodev.forumhub.domain.usuario;
+
+public record DadosAutenticacao(String login, String senha) {
+
+    
+}
