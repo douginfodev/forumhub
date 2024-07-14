7b8bb0c23d6604c306face0c3597093afd7904d5
= <=>
2024-07-13 21:11:12 -0300

    feat: Created SecurityConfigurations

--- src/main/java/com/douginfodev/forumhub/infra/exception/ErrorExceptions.java
+++ src/main/java/com/douginfodev/forumhub/infra/exception/ErrorExceptions.java
@@ -1,4 +1,4 @@
-package com.douginfodev.forumhub.infra;
+package com.douginfodev.forumhub.infra.exception;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
